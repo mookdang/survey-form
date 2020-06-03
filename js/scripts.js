86a8faca4ef52e85451463e936cc93aeb1265bea
mook <mookdang24@gmail.com>
2020-06-03 15:57:06 -0700

    add select type Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,11 @@ $(document).ready(function() {
   $("form#userName").submit(function(event) {
     event.preventDefault();
       const personNameInput = $("input#personName").val();
+      const foodType = $("input#foodType").val();
+
       $(".personName").text("personNameInput");
-      $("#output").text(personNameInput);
+      $("#output1").text(personNameInput);
+      $("#output2").text(foodType);
 
   });
 });
